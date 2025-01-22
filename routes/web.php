6660d92b04fc9d7ef824cb53e59499183bc2188d
Ruansyah14 <achmadlight@gmail.com>
2025-01-22 09:25:43 +0700

    ralat clear

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/post', \App\Http\Controllers\PostController::class);
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
